4c1e3864b0e82b5863419cf629c470d7c0f3f45e
roryhmillington <36569176+roryhmillington@users.noreply.github.com>
2018-03-06 12:25:14 -0800

    Add files via upload

---
+++ Exercises/Quadratic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadratic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the values of A");
+            double A = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the value of B");
+            double B = double.Parse(Console.ReadLine());
+            Console.WriteLine("Pleade enter the value of C");
+            double C = double.Parse(Console.ReadLine());
+
+            double positiveform = (-B + (Math.Sqrt(B * B - 4 * A * C))) / (2 * A);
+            Console.WriteLine($"X = (positive form)");
+            double negativeForm = (-B - (Math.Sqrt (B * B -4 * A * C))) / (2 * A);
+            Console.WriteLine($"X = (negativeForm)");
+
+        }
+    }
+}
