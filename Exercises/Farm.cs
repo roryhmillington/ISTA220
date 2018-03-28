9c3b70b9e10e71f713cd79d8e247e6d4162e3c15
roryhmillington <36569176+roryhmillington@users.noreply.github.com>
2018-03-27 19:11:58 -0700

    Add files via upload

---
+++ Exercises/Farm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheFarm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Horse horse = new Horse();
+            horse.Eats();
+            horse.Sleep();
+            
+
+            Cow cow = new Cow();
+            cow.Eats();
+            cow.Sleep();
+            
+            Pig pig = new Pig();
+            pig.Eats();
+            pig.Sleep();
+           
+
+            Chicken chicken = new Chicken();
+            chicken.Eats();
+            chicken.Sleep();
+
+            Console.ReadLine(); //requires the console to wait before closing the program
+                        
+        }
+    }
+}
